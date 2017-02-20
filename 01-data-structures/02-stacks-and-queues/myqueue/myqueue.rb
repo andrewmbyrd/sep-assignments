0ccd2a9129c41966651c4df70a4d8c48ae30f785
Andrew <andrewmbyrd@gmail.com>
2017-02-20 16:30:32 -0600

    Implemented stack and queue code

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,28 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[0]
   end
 
   def enqueue(element)
+    @queue[@queue.length] = element
+    @head = element
+    @tail = @queue[0]
   end
 
   def dequeue
+    temp = @queue[0]
+    for i in 0...(@queue.length-1)
+      @queue[i] = @queue[i + 1]
+    end
+    @queue.delete_at(@queue.length-1)
+    @tail = @queue[0]
+    @head = @queue[@queue.length-1]
+
+    return temp
   end
 
   def empty?
+    @queue.length == 0
   end
 end
