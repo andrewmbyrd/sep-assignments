51d50b231eaf383cbe0e251426a3af5ba90b874e
Andrew <andrewmbyrd@gmail.com>
2017-02-20 14:47:25 -0600

    implemented each of the line and marix data structures

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,18 +6,58 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    self.matrix = []
+    self.width = width
+    self.height = height
+    for i in 0...width
+      self.matrix[i] = []
+      for j in 0...height
+        self.matrix[i][j] = 0
+      end
+    end
   end
 
-  # Insert a Pixel at x, y
+  # Insert a Pixel at x, y, and really this should just be
+  # insert(Pixel) because its super confusing if x and y are anything
+  #other than pixel.x and pixel.y, respectively
   def insert(pixel, x, y)
+    if inbounds(x,y)
+      self.matrix[x][y] = pixel
+    else
+      raise BoundaryError "That is not a valid pixel location"
+    end
   end
 
   def at(x, y)
+    unless inbounds(x,y)
+      nil
+    else
+      return self.matrix[x][y]
+    end
+  end
+
+  def search(thing)
+    for i in 0...width
+      if matrix[i].index(thing)
+        return [i, matrix[i].index(thing)]
+      end
+    end
+
+    return nil
   end
 
   private
 
   def inbounds(x, y)
+    return x >= 0 && x < self.width && y >= 0 && y < self.height
   end
 
 end
+
+class BoundaryError < StandardError
+end
+
+#screen = Screen.new(5, 5)
+#screen.insert(Pixel.new(255,3,5,1,1), 1, 1)
+#puts screen.at(1,1)
+#puts screen.matrix
--- 01-data-structures/01-introduction-to-data-structures/screen/screen_spec.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen_spec.rb
@@ -2,20 +2,21 @@ require_relative 'screen'
 
 RSpec.describe Screen, type: Class do
   let(:screen) { Screen.new(10, 10) }
+  let(:pixel) { Pixel.new(255, 200, 175, 1, 1) }
 
   describe "#insert" do
     it "inserts a pixel at the proper x, y coordinates" do
-      pixel = Pixel.new(255, 200, 175, 1, 1)
-      screen.insert(p, 1, 1)
-      
+
+      screen.insert(pixel, 1, 1)
+
       expect(screen.at(1, 1)).to eq pixel
     end
 
     it "retains color information upon insertion" do
-      pixel = Pixel.new(255, 200, 175, 1, 1)
-      screen.insert(p, 1, 1)
+
+      screen.insert(pixel, 1, 1)
       p1 = screen.at(1, 1)
-      
+
       expect(p1.red).to eq pixel.red
       expect(p1.green).to eq pixel.green
       expect(p1.blue).to eq pixel.blue
@@ -26,13 +27,13 @@ RSpec.describe Screen, type: Class do
     it "returns the pixel at a specific location" do
       screen.insert(pixel, 1, 2)
       p1 = screen.at(1, 2)
-      
+
       expect(pixel).to eq p1
     end
 
     it "handles invalid x, y values gracefully" do
       pixel = screen.at(-1, -1)
-      
+
       expect(pixel).to eq nil
     end
   end
