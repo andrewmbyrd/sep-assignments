13c97c8f3ede22458382631b20b4329fbd94c9cf
Andrew <andrewmbyrd@gmail.com>
2017-02-22 17:32:32 -0600

    implemented the separate chaining portion of the hash code. Also added an answers file

--- 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
@@ -4,27 +4,81 @@ class LinkedList
   attr_accessor :head
   attr_accessor :tail
 
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+    @tail.next = node if @tail
+    @tail = node
+    @head = node unless head
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+    current_node = @head
+    if current_node == @tail
+      @head = nil
+      @tail = nil
+    else
+      while current_node.next
+        if current_node.next.next
+          current_node = current_node.next
+        else
+          current_node.next = nil
+          @tail = current_node
+        end
+      end
+    end
   end
 
   # This method prints out a representation of the list.
   def print
+    current_node = @head
+    while current_node
+      puts current_node.data
+      current_node = current_node.next
+    end
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
+
   def delete(node)
+    if node == @head
+      if @head == @tail
+        @head = nil
+        @tail = nil
+      else
+        @head = @head.next
+      end
+    else
+      current_node = @head
+      while current_node.next
+        if current_node.next == node
+          current_node.next = current_node.next.next
+         @tail = current_node if current_node.next == nil
+         break
+        end
+        current_node = current_node.next
+      end
+    end
   end
+
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+    node.next = @head
+    @head = node
+    @tail = node unless @tail
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+    temp = @head
+    @head = @head.next
+    @tail = nil if @head == nil
+    return temp
   end
 end
--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -3,31 +3,121 @@ require_relative 'linked_list'
 class SeparateChaining
   attr_reader :max_load_factor
 
+  #we'll go ahead and populate each space in the base array with an empty linked list from
+  # the outset. also keep track of num items to make calculating Load Factor easy
   def initialize(size)
     @max_load_factor = 0.7
+    @buckets = Array.new(size)
+    @buckets.each_with_index do |bucket, index|
+      @buckets[index] = LinkedList.new
+    end
+    @num_items = 0
   end
 
+  #simply always add the inserted item to the tail of the desired buckets. increment num_items
+  #resize when load factor gets too high
   def []=(key, value)
+
+    @buckets[index(key, size)].add_to_tail(Node.new(key, value))
+    @num_items += 1
+
+    resize if load_factor > @max_load_factor
   end
 
+  #traverse the current linked list until the desired element is found.
+  # raise an error if an invalid key is sent
   def [](key)
+    current_node = @buckets[index(key, size)].head
+    if !current_node
+      raise InvalidKeyError "Cannot retrieve that item - not instantiated"
+    end
+    while current_node.key != key
+      current_node = current_node.next
+      break unless current_node
+    end
+
+    if !current_node
+      raise InvalidKeyError "Cannot retrieve that item - not instantiated"
+    end
+
+    return current_node.value
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    key.sum % size
   end
 
   # Calculate the current load factor
   def load_factor
+    Float(@num_items) / size
   end
 
-  # Simple method to return the number of items in the hash
+  # Simple method to return the number of BUCKETS in the hash
   def size
+    @buckets.length
   end
 
+  def print
+    @buckets.each_with_index do |bucket, index|
+      if bucket.head
+        space=" "
+        space = "  " if index < 10
+        puts "Bucket#{space}#{index} \n"
+        current_node = bucket.head
+        while current_node
+          puts "Key: #{current_node.key}\n"
+          puts "Value: #{current_node.value}\n"
+          if current_node.next
+            puts " -- > "
+          else
+            puts "\n\n"
+          end
+          current_node = current_node.next
+        end
+      end
+
+    end
+    puts "Load Factor: #{load_factor}"
+  end
+  
   # Resize the hash
   def resize
+    more_buckets = Array.new(size * 2, LinkedList.new)
+    more_buckets.each_with_index do |bucket, index|
+      more_buckets[index] = LinkedList.new
+    end
+    @buckets.each do |bucket|
+      current_node = bucket.head
+      #puts "Current Node Key: #{current_node.key}"
+      #puts "Next node key: #{current_node.next.key} \n\n"
+      if current_node
+        more_buckets[index(current_node.key, more_buckets.length)].add_to_tail(current_node)
+        current_node = current_node.next
+      end
+    end
+
+    @buckets = more_buckets
+  end
+
+
+
+  class InvalidKeyError < StandardError
   end
 end
+
+class InvalidKeyError < StandardError
+end
+
+star_wars_movies = SeparateChaining.new(6)
+
+star_wars_movies["Star Wars: The Phantom Menace"] = "Number One"
+star_wars_movies["Star Wars: Attack of the Clones"] = "Number Two"
+star_wars_movies["Star Wars: Revenge of the Sith"] = "Number Three"
+star_wars_movies["Star Wars: A New Hope"] = "Number Four"
+star_wars_movies["Star Wars: The Empire Strikes Back"] = "Number Five"
+star_wars_movies["Star Wars: Return of the Jedi"] = "Number Six"
+star_wars_movies["Lotr"]
+star_wars_movies.print
