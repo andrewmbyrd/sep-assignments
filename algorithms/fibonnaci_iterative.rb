3cf199419097fd38b6ae5a51c33c99424f6f6875
Andrew <andrewmbyrd@gmail.com>
2017-03-06 12:04:02 -0600

    intro to algorithms solutions

---
+++ algorithms/fibonnaci_iterative.rb
@@ -0,0 +1,22 @@
+def fib(n)
+
+  if n < 3
+    return n - 1
+  else
+
+    fibs=[]
+    fibs[0] = 0
+    fibs[1] = 1
+
+    for i in 2...n
+      fibs[i] = fibs[i - 1] + fibs[i - 2]
+    end
+
+    return fibs[n - 1]
+  end
+
+end
+
+for num in 1..10
+  puts fib(num)
+end
